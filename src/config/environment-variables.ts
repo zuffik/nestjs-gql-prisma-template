2d8f8f7668b9239b815e0114e9c7687d9e36184d
Kristián Žuffa <kristian.zuffa@gmail.com>
2023-07-11 12:35:46 +0200

    config

---
+++ src/config/environment-variables.ts
@@ -0,0 +1,31 @@
+import {IsEnum, IsInt, IsString} from "class-validator";
+import {Environment} from "./environment";
+
+export class EnvironmentVariables {
+  @IsEnum(Environment)
+  NODE_ENV: Environment;
+
+  @IsInt()
+  APP_PORT: number;
+
+  @IsString()
+  DB_PROVIDER: string;
+
+  @IsString()
+  DB_HOST: string;
+
+  @IsInt()
+  DB_PORT: number;
+
+  @IsString()
+  DB_USER: string;
+
+  @IsString()
+  DB_PASS: string;
+
+  @IsString()
+  DB_NAME: string;
+
+  @IsString()
+  DB_SCHEMA: string;
+}
